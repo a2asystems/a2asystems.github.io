2014ebfc4ae8b9868604b19b580e47a7286deffb
a2asystems <info@a2asystems.net>
2026-06-02 05:33:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function setPers(name) {
 
 // ── BOOT ───────────────────────────────────────────────────────────────────
 // Build-Timestamp wird beim Deploy eingefügt — für Auto-Reload-Mechanismus
-var APP_BUILD = 1780371089;
+var APP_BUILD = 1780371228;
 
 window.addEventListener('resize', () => { if(L) drawChart(L); });
 
